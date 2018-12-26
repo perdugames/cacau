6e214f9ca64e1d433e653d234bf2a0cf632d7fec
perdugames <perdugames@gmail.com>
2018-12-26 14:05:23 -0200

    Test async OK

--- tests/unit/runner_test.js
+++ tests/unit/runner_test.js
@@ -266,153 +266,238 @@ TEST_ASYNC('TestCreateSuiteCheckAfterAllAlterVarsFixture', (done) => {
     };
     runner.run(callbackRunProgress);
 });
-//     
-//    TEST_F('TestCreateSuiteCheckBeforeEarchAlterVarsFixture', () => {
-//        const runner = new Runner();
-//        const rootSuite = runner.rootSuite;
-//
-//        const suite1 = runner.createSuite('Suite 1');
-//        rootSuite.addChild(suite1);
-//        
-//        suite1.varsFixture = { x: 0 };
-//        suite1.beforeEach = function(varsFixture) {
-//            varsFixture.x = 0;
-//        }
-//        
-//        const test1 = runner.createTest('Test 1', function() {
-//            this.x++;
-//            CHECK_ACTUAL_EQUAL_EXPECTED_OBJECT(this.x, 1);
-//        });
-//        suite1.addChild(test1);
-//    
-//        const test2 = runner.createTest('Test 2', function() {
-//            this.x++;
-//            CHECK_ACTUAL_EQUAL_EXPECTED_OBJECT(this.x, 1);
-//        });
-//        suite1.addChild(test2);
-//
-//        const result = runner.run();
-//
-//        CHECK_ACTUAL_EQUAL_EXPECTED_OBJECT(result, []);
-//    }),
-//     
-//    TEST_F('TestCreateSuiteCheckBeforeEarchRecursiveAlterVarsFixture', () => {
-//        const runner = new Runner();
-//        const rootSuite = runner.rootSuite;
-//        
-//        const suite1 = runner.createSuite('Suite 1');
-//        rootSuite.addChild(suite1);
-//        
-//        suite1.varsFixture = { x: 0 };
-//        suite1.beforeEach = function(varsFixture) {
-//            varsFixture.x = 0;
-//        }
-//        const test1 = runner.createTest('Test 1', function() {
-//            this.x++;
-//            CHECK_ACTUAL_EQUAL_EXPECTED_OBJECT(this.x, 1);
-//        });
-//        suite1.addChild(test1);
-//        const test2 = runner.createTest('Test 2', function() {
-//            this.x++;
-//            CHECK_ACTUAL_EQUAL_EXPECTED_OBJECT(this.x, 1);
-//        });
-//        suite1.addChild(test2);
-//    
-//    
-//        const suite2 = runner.createSuite('Suite 2');
-//        suite1.addChild(suite2);
-//    
-//        suite2.varsFixture = { y: 0 };
-//        suite2.beforeEach = function(varsFixture) {
-//            varsFixture.x++;
-//        }
-//        const suite2Test1 = runner.createTest('Test 1', function() {
-//            this.x++;
-//            CHECK_ACTUAL_EQUAL_EXPECTED_OBJECT(this.x, 2);
-//        });
-//        suite2.addChild(suite2Test1);
-//    
-//    
-//        const result = runner.run();
-//
-//        CHECK_ACTUAL_EQUAL_EXPECTED_OBJECT(result, []);
-//    }),
-//     
-//    TEST_F('TestCreateSuiteCheckAfterEarchAlterVarsFixture', () => {
-//        const runner = new Runner();
-//        const rootSuite = runner.rootSuite;
-//
-//        const suite1 = runner.createSuite('Suite 1');
-//        rootSuite.addChild(suite1);
-//        
-//        suite1.varsFixture = { x: 0 };
-//        suite1.afterEach = function(varsFixture) {
-//            varsFixture.x = 0;
-//        }
-//        
-//        const test1 = runner.createTest('Test 1', function() {
-//            this.x++;
-//            CHECK_ACTUAL_EQUAL_EXPECTED_OBJECT(this.x, 1);
-//        });
-//        suite1.addChild(test1);
-//    
-//        const test2 = runner.createTest('Test 2', function() {
-//            this.x++;
-//            CHECK_ACTUAL_EQUAL_EXPECTED_OBJECT(this.x, 1);
-//        });
-//        suite1.addChild(test2);
-//
-//        const result = runner.run();
-//
-//        CHECK_ACTUAL_EQUAL_EXPECTED_OBJECT(result, []);
-//    }),
-//     
-//    TEST_F('TestCreateSuiteCheckAfterEarchRecursiveAlterVarsFixture', () => {
-//        const runner = new Runner();
-//        const rootSuite = runner.rootSuite;
-//        
-//        const suite1 = runner.createSuite('Suite 1');
-//        rootSuite.addChild(suite1);
-//        
-//        suite1.varsFixture = { x: 0 };
-//        suite1.afterEach = function(varsFixture) {
-//            varsFixture.x = 0;
-//        }
-//        const test1 = runner.createTest('Test 1', function() {
-//            this.x++;
-//            CHECK_ACTUAL_EQUAL_EXPECTED_OBJECT(this.x, 1);
-//        });
-//        suite1.addChild(test1);
-//        const test2 = runner.createTest('Test 2', function() {
-//            this.x++;
-//            CHECK_ACTUAL_EQUAL_EXPECTED_OBJECT(this.x, 1);
-//        });
-//        suite1.addChild(test2);
-//    
-//    
-//        const suite2 = runner.createSuite('Suite 2');
-//        suite1.addChild(suite2);
-//    
-//        suite2.varsFixture = { y: 0 };
-//        suite2.afterEach = function(varsFixture) {
-//            varsFixture.x++;
-//        }
-//        const suite2Test1 = runner.createTest('Test 1', function() {
-//            this.x++;
-//            CHECK_ACTUAL_EQUAL_EXPECTED_OBJECT(this.x, 1);
-//        });
-//        suite2.addChild(suite2Test1);
-//    
-//        const suite2Test2 = runner.createTest('Test 2', function() {
-//            this.x++;
-//            CHECK_ACTUAL_EQUAL_EXPECTED_OBJECT(this.x, 2);
-//        });
-//        suite2.addChild(suite2Test2);
-//    
-//    
-//        const result = runner.run();
-//
-//        CHECK_ACTUAL_EQUAL_EXPECTED_OBJECT(result, []);
-//    }),
-//    
-//);
+
+TEST_ASYNC('TestCreateSuiteRecursiveCheckOrderAfterAll', (done) => {
+    const runner = new Runner();
+    const rootSuite = runner.rootSuite;
+    
+    const suite1 = runner.createSuite('Suite 1');
+    rootSuite.addChild(suite1);
+    suite1.varsFixture = { x: true };
+    suite1.afterAll = function(varsFixture) {
+        varsFixture.x = false;
+    }
+    const test1 = runner.createTest('Test 1', function() {
+        CHECK_TRUE(this.x);
+    });
+    suite1.addChild(test1);
+    
+    const suite1Suite2 = runner.createSuite('Suite 1 - Suite 2');
+    suite1.addChild(suite1Suite2);
+    suite1Suite2.varsFixture = { y: 9 };
+    suite1Suite2.afterAll = function(varsFixture) {
+        varsFixture.x = false;
+    }
+    const suite2Test1 = runner.createTest('Suite 1 - Suite 2 - Test 1', function() {
+        CHECK_TRUE(this.x);
+    });
+    suite1Suite2.addChild(suite2Test1);
+
+    const callbackRunProgress = () => {
+        if(runner.getRunProgress() === 100)
+           done(() => CHECK_FALSE(suite1.varsFixture.x));
+    };
+    runner.run(callbackRunProgress);
+});
+
+TEST_ASYNC('TestCreateSuiteRecursiveTestSuiteTestSuiteCheckOrderAfterAll', (done) => {
+    const runner = new Runner();
+    const rootSuite = runner.rootSuite;
+    let spyX = 0;
+    
+    const suite1 = runner.createSuite('Suite 1');
+    rootSuite.addChild(suite1);
+    suite1.afterAll = function(varsFixture) {
+        spyX = 2;
+//        console.log(this.name);
+    }
+    const test1 = runner.createTest('Suite 1 - Test 1', function() {
+        CHECK_ACTUAL_EQUAL_EXPECTED(spyX, 0);
+    });
+    suite1.addChild(test1);
+    
+    const suite1Suite2 = runner.createSuite('Suite 1 - Suite 2');
+    suite1.addChild(suite1Suite2);
+    suite1Suite2.afterAll = function(varsFixture) {
+        spyX = 1;
+//        console.log(this.name);
+    }
+    const suite2Test1 = runner.createTest('Suite 1 - Suite 2 - Test 1', function() {
+        CHECK_ACTUAL_EQUAL_EXPECTED(spyX, 0);
+    });
+    suite1Suite2.addChild(suite2Test1);
+    
+    const test2 = runner.createTest('Suite 1 - Test 2', function() {
+        CHECK_ACTUAL_EQUAL_EXPECTED(spyX, 1);
+    });
+    suite1.addChild(test2);
+    
+    
+    const suite2 = runner.createSuite('Suite 2');
+    rootSuite.addChild(suite2);
+    suite2.afterAll = function(varsFixture) {
+        spyX = 3;
+//        console.log(this.name);
+    }
+
+    const callbackRunProgress = () => {
+        if(runner.getRunProgress() === 100)
+           done(() => CHECK_ACTUAL_EQUAL_EXPECTED(spyX, 3));
+    };
+    runner.run(callbackRunProgress);
+});
+
+TEST_ASYNC('TestCreateSuiteCheckBeforeEarchAlterVarsFixture', (done) => {
+    const runner = new Runner();
+    const rootSuite = runner.rootSuite;
+
+    const suite1 = runner.createSuite('Suite 1');
+    rootSuite.addChild(suite1);
+
+    suite1.varsFixture = { x: 0 };
+    suite1.beforeEach = function(varsFixture) {
+        varsFixture.x = 0;
+    }
+    
+    const test1 = runner.createTest('Test 1', function() {
+        this.x++;
+        CHECK_ACTUAL_EQUAL_EXPECTED_OBJECT(this.x, 1);
+    });
+    suite1.addChild(test1);
+    
+    const test2 = runner.createAsyncTest('Test 2', function(testDone) {
+        this.x++;
+        testDone(() => CHECK_ACTUAL_EQUAL_EXPECTED_OBJECT(this.x, 1));
+    });
+    suite1.addChild(test2);
+
+    const callbackRunProgress = () => {
+        if(runner.getRunProgress() === 100)
+           done(() => CHECK_ACTUAL_EQUAL_EXPECTED(runner.result.failing, 0));
+    };
+    runner.run(callbackRunProgress);
+});
+
+TEST_ASYNC('TestCreateSuiteCheckBeforeEarchRecursiveAlterVarsFixture', (done) => {
+    const runner = new Runner();
+    const rootSuite = runner.rootSuite;
+
+    const suite1 = runner.createSuite('Suite 1');
+    rootSuite.addChild(suite1);
+
+    suite1.varsFixture = { x: 0 };
+    suite1.beforeEach = function(varsFixture) {
+        varsFixture.x = 0;
+    }
+    const test1 = runner.createTest('Test 1', function() {
+        this.x++;
+        CHECK_ACTUAL_EQUAL_EXPECTED(this.x, 1);
+    });
+    suite1.addChild(test1);
+    const test2 = runner.createTest('Test 2', function() {
+        this.x++;
+        CHECK_ACTUAL_EQUAL_EXPECTED(this.x, 1);
+    });
+    suite1.addChild(test2);
+
+    const suite2 = runner.createSuite('Suite 2');
+    suite1.addChild(suite2);
+
+    suite2.varsFixture = { y: 0 };
+    suite2.beforeEach = function(varsFixture) {
+        varsFixture.x++;
+    }
+    const suite2Test1 = runner.createTest('Test 1', function() {
+        this.x++;
+        CHECK_ACTUAL_EQUAL_EXPECTED(this.x, 2);
+    });
+    suite2.addChild(suite2Test1);
+
+    const callbackRunProgress = () => {
+        if(runner.getRunProgress() === 100)
+           done(() => CHECK_ACTUAL_EQUAL_EXPECTED(runner.result.failing, 0));
+    };
+    runner.run(callbackRunProgress);
+});
+
+TEST_ASYNC('TestCreateSuiteCheckAfterEarchAlterVarsFixture', (done) => {
+    const runner = new Runner();
+    const rootSuite = runner.rootSuite;
+
+    const suite1 = runner.createSuite('Suite 1');
+    rootSuite.addChild(suite1);
+
+    suite1.varsFixture = { x: 0 };
+    suite1.afterEach = function(varsFixture) {
+        varsFixture.x = 0;
+    }
+
+    const test1 = runner.createTest('Test 1', function() {
+        this.x++;
+        CHECK_ACTUAL_EQUAL_EXPECTED(this.x, 1);
+    });
+    suite1.addChild(test1);
+
+    const test2 = runner.createTest('Test 2', function() {
+        this.x++;
+        CHECK_ACTUAL_EQUAL_EXPECTED(this.x, 1);
+    });
+    suite1.addChild(test2);
+
+    const callbackRunProgress = () => {
+        if(runner.getRunProgress() === 100)
+           done(() => CHECK_ACTUAL_EQUAL_EXPECTED(runner.result.failing, 0));
+    };
+    runner.run(callbackRunProgress);
+});
+
+TEST_ASYNC('TestCreateSuiteCheckAfterEarchRecursiveAlterVarsFixture', (done) => {
+    const runner = new Runner();
+    const rootSuite = runner.rootSuite;
+
+    const suite1 = runner.createSuite('Suite 1');
+    rootSuite.addChild(suite1);
+
+    suite1.varsFixture = { x: 0 };
+    suite1.afterEach = function(varsFixture) {
+        varsFixture.x++;
+    }
+    const test1 = runner.createTest('Test 1', function() {
+        this.x++;
+        CHECK_ACTUAL_EQUAL_EXPECTED(this.x, 1);
+    });
+    suite1.addChild(test1);
+    const test2 = runner.createTest('Test 2', function() {
+        this.x++;
+        CHECK_ACTUAL_EQUAL_EXPECTED(this.x, 3);
+    });
+    suite1.addChild(test2);
+
+
+    const suite2 = runner.createSuite('Suite 2');
+    suite1.addChild(suite2);
+
+    suite2.varsFixture = { y: 0 };
+    suite2.afterEach = function(varsFixture) {
+        varsFixture.x = 0;
+    }
+
+    const suite2Test1 = runner.createTest('Test 1', function() {
+        this.x++;
+        CHECK_ACTUAL_EQUAL_EXPECTED(this.x, 5);
+    });
+    suite2.addChild(suite2Test1);
+
+    const suite2Test2 = runner.createAsyncTest('Test 2', function(testDone) {
+        this.x++;
+        testDone(() => CHECK_ACTUAL_EQUAL_EXPECTED(this.x, 2));
+    });
+    suite2.addChild(suite2Test2);
+
+
+    const callbackRunProgress = () => {
+        if(runner.getRunProgress() === 100)
+           done(() => CHECK_ACTUAL_EQUAL_EXPECTED(runner.result.failing, 0));
+    };
+    runner.run(callbackRunProgress);
+});
